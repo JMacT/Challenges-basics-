0b4b045c18526486e4882743d31c0e51cf25a234
JMacT <jessemacfarlane@gmail.com>
2018-03-09 13:46:27 -0600

    Lunch break noodling
    Tried to get the cipher to ignore spaces but so far failed. No decision on what to do with non-letters yet. No "unscramble" feature yet.

--- Reddit Easy Challange 3 - Caesar Cipher/main.cpp
+++ Reddit Easy Challange 3 - Caesar Cipher/main.cpp
@@ -18,7 +18,7 @@ using namespace std;
 This is the plan:
 http://www.cplusplus.com/forum/general/74820/
 */
-void fixStr(char*, char*);
+void fixStr(char*, char*, int);
 void caesar_Cipher(char*, int, int);
 
 
@@ -26,7 +26,7 @@ void caesar_Cipher(char*, int, int);
 int main()
 {
 
-    int cipher_Val = 12; //How many chars do you want to shift by?
+    int cipher_Val = 1; //How many chars do you want to shift by?
     char TEMP[100];
     //this holds the string in an array format for crawling later.
     char new_String[100];
@@ -34,10 +34,10 @@ int main()
     cout << "You should enter a string. I'll spit out what I read." << endl;
     scanf("%[^\n]*c", TEMP);
 
-    fixStr(&TEMP[0], &new_String[0]);
+    fixStr(&TEMP[0], &new_String[0], cipher_Val);
 
     //caesar_Cipher(&new_String[0], cipher_Val, strlen(new_String));
-
+    cout << new_String;
 }
 
 
@@ -51,19 +51,45 @@ void caesar_Cipher(char*str, int cipher, int str_Leng){
 
     return;
 };
-void fixStr(char *source,char *dest){
+void fixStr(char *source,char *dest, int cipher){
     int i=0;
+    char val;
+    int val_int=0;
+    int _bool = 0; //Is this a letter that needs to be cipher'd?
 
     for(i=0; i<100; i++){
-        if(*(source+i) != '\0'){
-            *(dest+0) = *(source+i); //HOW DO I ADD TO THE VALS?
-            cout << *(dest+i);
+        _bool = 0;
+
+        val = *(source+i); //Use an easier variable to edit in-line.
+        val_int = *(source+i);
+
+        if(val != '\0'){
+            if ((val_int >= 97) && (val_int <= 122)){
+                _bool = 1;
+
+            }
+            if ( (val_int >= 65) && (val_int <= 90)){
+                _bool = 1;
+
+            }
+            if (_bool = 1){
+                val = val_int + cipher; //If it is a valid letter, apply the cipher
+            }
+            else{
+                if ( val_int != 32 ){
+                    while( val_int < 65){
+                        val += 1;
+                    };
+                };
+            };
+
+            *(dest+i) = val;
         }
 
         else{
+            *(dest+i) = '\0';
             i=100;
         };
     };
-    cout << endl << "this is the test of source+1: " << (source+2) << endl;
 };
 
